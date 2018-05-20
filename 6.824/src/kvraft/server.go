6362808f2e51543bd45efa19b88db76396635628
LsmAtPackRat <drliusm@163.com>
2018-05-20 10:30:56 +0800

    Clean the code, still need some optimization, such as leader cache.

--- 6.824/src/kvraft/server.go
+++ 6.824/src/kvraft/server.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"raft"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -37,23 +38,23 @@ type Op struct {
 }
 
 type ResultItem struct {
-	reply         interface{}   // maybe GetReply or PutAppendReply
-	term          int           // the term in which the leader Start() this Op.
-	serial_number int64         // indicate which Op is the owner of this result.
+	reply         interface{} // maybe GetReply or PutAppendReply
+	term          int         // the term in which the leader Start() this Op.
+	serial_number int64       // indicate which Op is the owner of this result.
 }
 
 type KVServer struct {
-	mu        sync.Mutex
-	me        int
-	rf        *raft.Raft
-	applyCh   chan raft.ApplyMsg
+	mu      sync.Mutex
+	me      int
+	rf      *raft.Raft
+	applyCh chan raft.ApplyMsg
 
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
-	kvmappings    map[string]string         // the store is based on this map.
-	waitMap       map[int][]chan ResultItem // index -> channels
-	servedRequest map[int64]interface{}     // record which command is served before, and the value is the reply.
+	kvmappings    map[string]string       // the store is based on this map.
+	waitCh        map[int]chan ResultItem // index -> channel
+	servedRequest map[int64]interface{}   // record which command is served before, and the value is the reply.
 }
 
 // Get RPC handler. You should enter an Op in the Raft log using Start().
@@ -78,39 +79,12 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	}
 	// we need to update some data structures so that apply knows to poke us later.
 	wait_ch := make(chan ResultItem, 10)
-	kv.waitMap[index] = append(kv.waitMap[index], wait_ch)
+	kv.waitCh[index] = wait_ch
 	kv.mu.Unlock()
 
 	// start a goroutine to check whether the term is changed?
-	go func() {
-		for {
-			kv.mu.Lock()
-			curr_term, curr_isleader := kv.rf.GetState()
-			if curr_term != term || !curr_isleader {
-				// notify the server to return.
-				// construct an error result and send to waitMap channel.
-				var result_item ResultItem
-				result_item.term = curr_term
-				var reply GetReply
-				reply.WrongLeader = true
-				result_item.reply = reply
-				if _, ok := kv.waitMap[index]; ok {
-					// apply() haven't write to the channel.
-					delete(kv.waitMap, index) // apply will not write to this channel.
-					wait_ch <- result_item
-				}
-				kv.mu.Unlock()
-				return
-			} else {
-				if _, ok := kv.waitMap[index]; !ok {
-					kv.mu.Unlock()
-					return
-				}
-			}
-			kv.mu.Unlock()
-			time.Sleep(time.Millisecond * time.Duration(50))
-		}
-	}()
+	done_check_leadership := int32(0)
+	go kv.checkLeadership(index, term, &done_check_leadership)
 
 	// wait for apply() to poke us.
 	result_item := (<-wait_ch)
@@ -118,10 +92,11 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 		reply.WrongLeader = true
 	} else {
 		temp_reply := (result_item.reply).(GetReply)
-		reply.WrongLeader = temp_reply.WrongLeader
+		reply.WrongLeader = false
 		reply.Err = temp_reply.Err
 		reply.Value = temp_reply.Value
 	}
+	atomic.StoreInt32(&done_check_leadership, 1) // I'm not sure, whether a w/r tear could happen without atomic. It's up to Go's memory model.
 	return
 }
 
@@ -129,74 +104,73 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	DPrintf("server.go - server-%d PutAppend("+args.Key+", "+args.Value+", "+args.Op+")", kv.me)
 	// Your code here.
-	if args.Op == "Put" || args.Op == "Append" {
+	kv.mu.Lock()
+	key := args.Key
+	value := args.Value
+	var op Op
+	op.Key = key
+	op.Value = value
+	op.SerialNumber = args.SerialNumber
+	if args.Op == "Put" {
+		op.Type = CmdPut
+	} else {
+		op.Type = CmdAppend
+	}
+	index, term, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		kv.mu.Unlock()
+		return
+	}
+
+	// we need to update some data structures so that apply knows to poke us later.
+	wait_ch := make(chan ResultItem, 10)
+	kv.waitCh[index] = wait_ch // cover the previous mapping if exists.
+	kv.mu.Unlock()
+
+	// start a goroutine to check whether the term is changed?
+	done_check_leadership := int32(0)
+	go kv.checkLeadership(index, term, &done_check_leadership)
+	// the leader could not be the leader any more.
+	// wait for apply() to poke us.
+	result_item := (<-wait_ch)
+	if result_item.serial_number != op.SerialNumber || result_item.term != term {
+		reply.WrongLeader = true
+	} else {
+		temp_reply := (result_item.reply).(PutAppendReply)
+		reply.Err = temp_reply.Err
+		reply.WrongLeader = false
+	}
+	atomic.StoreInt32(&done_check_leadership, 1)
+}
+
+// check whether the server lose its leadership in an infinate loop.
+func (kv *KVServer) checkLeadership(index int, term int, done *int32) {
+	for atomic.LoadInt32(done) == 0 {
 		kv.mu.Lock()
-		key := args.Key
-		value := args.Value
-		var op Op
-		op.Key = key
-		op.Value = value
-		op.SerialNumber = args.SerialNumber
-		if args.Op == "Put" {
-			op.Type = CmdPut
-		} else {
-			op.Type = CmdAppend
-		}
-		index, term, isLeader := kv.rf.Start(op)
-		if !isLeader {
-			reply.WrongLeader = true
+		curr_term, curr_isleader := kv.rf.GetState()
+		if curr_term != term || !curr_isleader {
+			// notify the server to return.
+			// construct an error result and send to waitMap channel.
+			var result_item ResultItem
+			result_item.term = curr_term
+			if channel, ok := kv.waitCh[index]; ok {
+				// apply() haven't write to the channel.
+				delete(kv.waitCh, index) // apply will not write to this channel.
+				channel <- result_item
+			}
 			kv.mu.Unlock()
 			return
-		}
-
-		// we need to update some data structures so that apply knows to poke us later.
-		wait_ch := make(chan ResultItem, 10)
-		kv.waitMap[index] = append(kv.waitMap[index], wait_ch)
-		kv.mu.Unlock()
-
-		// start a goroutine to check whether the term is changed?
-		go func() {
-			for {
-				kv.mu.Lock()
-				curr_term, curr_isleader := kv.rf.GetState()
-				if curr_term != term || !curr_isleader {
-					// notify the server to return.
-					// construct an error result and send to waitMap channel.
-					var result_item ResultItem
-					result_item.term = curr_term
-					var reply GetReply
-					reply.WrongLeader = true
-					result_item.reply = reply
-					if _, ok := kv.waitMap[index]; ok {
-						// apply() haven't write to the channel.
-						delete(kv.waitMap, index) // apply will not write to this channel.
-						wait_ch <- result_item
-					}
-					kv.mu.Unlock()
-					return
-				} else {
-					if _, ok := kv.waitMap[index]; !ok {
-						kv.mu.Unlock()
-						return
-					}
-				}
-				kv.mu.Unlock()
-				time.Sleep(time.Millisecond * time.Duration(50))
-			}
-		}()
-		// the leader could not be the leader any more.
-		// wait for apply() to poke us.
-		result_item := (<-wait_ch)
-		if result_item.serial_number != op.SerialNumber || result_item.term != term {
-			reply.WrongLeader = true
 		} else {
-			temp_reply := (result_item.reply).(PutAppendReply)
-			reply.Err = temp_reply.Err
-			reply.WrongLeader = temp_reply.WrongLeader
+			if _, ok := kv.waitCh[index]; !ok {
+				kv.mu.Unlock()
+				return
+			}
 		}
-	} else {
-		log.Fatal("PutAppend() get a wrong args!")
+		kv.mu.Unlock()
+		time.Sleep(time.Millisecond * time.Duration(50))
 	}
+
 }
 
 //
@@ -235,7 +209,7 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	// You may need initialization code here.
 	kv.kvmappings = make(map[string]string)
-	kv.waitMap = make(map[int][]chan ResultItem)
+	kv.waitCh = make(map[int]chan ResultItem)
 	kv.servedRequest = make(map[int64]interface{})
 
 	kv.applyCh = make(chan raft.ApplyMsg)
@@ -278,14 +252,13 @@ func (kv *KVServer) apply(command_index int, command_term int, command interface
 	}
 
 	cmd_type := op.Type
+	var result_item ResultItem
 	switch cmd_type {
 	case CmdGet:
 		// NOTE: if we don't unlock() before write to the channel, it'll cause deadlock.
 		// if other kvserver call kv.rf.Start(), this kvserver will call  apply() too.
 		// then it could block here with a lock held in hand. But Get() should get the lock and then unlock and then read the channel, so, deadlock!
 		// check whether to poke the Get().
-		var result_item ResultItem
-		result_item.term = command_term
 		var reply GetReply
 		if prev_reply, ok := kv.servedRequest[op.SerialNumber]; ok {
 			result_item.reply = prev_reply
@@ -294,20 +267,12 @@ func (kv *KVServer) apply(command_index int, command_term int, command interface
 			if ok {
 				reply.Value = value
 				reply.Err = OK
-				reply.WrongLeader = false
 			} else {
 				reply.Value = ""
 				reply.Err = ErrNoKey
-				reply.WrongLeader = false
 			}
 			kv.servedRequest[op.SerialNumber] = reply
 			result_item.reply = reply
-		}
-		result_item.serial_number = op.SerialNumber
-		// send results to all of the channels block for this index.
-		for _, channel := range kv.waitMap[command_index] {
-			channel <- result_item
-			delete(kv.waitMap, command_index)
 		}
 	case CmdPut:
 		if _, ok := kv.servedRequest[op.SerialNumber]; !ok {
@@ -316,17 +281,9 @@ func (kv *KVServer) apply(command_index int, command_term int, command interface
 			kv.kvmappings[op.Key] = op.Value
 			kv.servedRequest[op.SerialNumber] = true
 		}
-		var result_item ResultItem
-		result_item.term = command_term
 		var reply PutAppendReply
 		reply.Err = OK
-		reply.WrongLeader = false
 		result_item.reply = reply
-		result_item.serial_number = op.SerialNumber
-		for _, channel := range kv.waitMap[command_index] {
-			channel <- result_item
-			delete(kv.waitMap, command_index)
-		}
 	case CmdAppend:
 		if _, ok := kv.servedRequest[op.SerialNumber]; !ok {
 			// we haven't serve for this command. apply this command.
@@ -339,16 +296,15 @@ func (kv *KVServer) apply(command_index int, command_term int, command interface
 			}
 			kv.servedRequest[op.SerialNumber] = true
 		}
-		var result_item ResultItem
-		result_item.term = command_term
 		var reply PutAppendReply
 		reply.Err = OK
-		reply.WrongLeader = false
 		result_item.reply = reply
-		result_item.serial_number = op.SerialNumber
-		for _, channel := range kv.waitMap[command_index] {
-			channel <- result_item
-			delete(kv.waitMap, command_index)
-		}
+	}
+
+	result_item.term = command_term
+	result_item.serial_number = op.SerialNumber
+	if channel, ok := kv.waitCh[command_index]; ok {
+		channel <- result_item
+		delete(kv.waitCh, command_index)
 	}
 }
